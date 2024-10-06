725d69f8414ce50b97f9231cb17e4eacb7d97d46
LAPTOP-03LP18JT\PC <susurikitami@gmail.com>
2024-10-06 14:50:25 +0700

    init productDAO and CategoryDAO

---
+++ src/java/entity/Product.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+public class Product {
+    private int id;
+    private String name;
+    private String imgage;
+    private int quantity;
+    private float price;
+    private String description;
+    private int categoryId;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String imgage, int quantity, float price, String description, int categoryId) {
+        this.id = id;
+        this.name = name;
+        this.imgage = imgage;
+        this.quantity = quantity;
+        this.price = price;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgage() {
+        return imgage;
+    }
+
+    public void setImgage(String imgage) {
+        this.imgage = imgage;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
+    
+    
+    
+    
+    
+}
